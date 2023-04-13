2c2cd0dba18fa78628c263cc22be57b62211d12b
Hemantg56 <hemantgadagi56@gmail.com>
2023-04-13 15:38:52 +0530

    1

---
+++ extener.js
@@ -0,0 +1,12 @@
+// window.alert('waring');
+// window.confirm(' daddy daddy');
+// window.prompt(' who is your daddy');
+// window.print('print');
+
+
+
+// let name = prompt('your name please', "guest")
+// console.log(name)
+// let name = prompt('what are you', 'parth')
+// console.log(name)
+// isme bhi prompt de skate hai but apane ko by defolt name ya console me name lana hai to ese late hai
